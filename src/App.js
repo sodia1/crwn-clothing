028e7a6ff940507f9e4f6218d11efe5d8966e0a8
sodia1 <gauravsodia@gmail.com>
2020-01-08 14:19:19 +0530

    update our menu-items to navigate withRouter HOC

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
